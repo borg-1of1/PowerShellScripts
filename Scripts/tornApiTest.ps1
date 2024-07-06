ab7312e1d29bd42f31e075195fb3a97a2be585a6
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2024-07-06 16:10:13 -0500

    cleaned out old info

--- Scripts/tornApiTest.ps1
+++ Scripts/tornApiTest.ps1
@@ -1,8 +1,8 @@
-﻿$response = Invoke-WebRequest -Uri https://api.torn.com/faction/?selections="&"key=BJWrMGwOjuSMHvMd -UseBasicParsing
+﻿$response = Invoke-WebRequest -Uri https://api.torn.com/faction/?selections="&"key= -UseBasicParsing
 $factionInfo = $response | ConvertFrom-Json
 foreach ($member in $factionInfo.members)
 {    
-    $memberInfo = Invoke-WebRequest -Uri https://api.torn.com/user/"$member"?selections="&"key=BJWrMGwOjuSMHvMd -UseBasicParsing
+    $memberInfo = Invoke-WebRequest -Uri https://api.torn.com/user/"$member"?selections="&"key= -UseBasicParsing
     $userInfo = $memberInfo.Content | ConvertFrom-Json
     $userInfo
 }
