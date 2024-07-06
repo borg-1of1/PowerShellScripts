07157482610ef5907998adf98614ec19cf9de803
borg-1of1 <56663946+borg-1of1@users.noreply.github.com>
2024-07-06 16:17:28 -0500

    Delete Scripts/tornApiTest.ps1
    Signed-off-by: borg-1of1 <56663946+borg-1of1@users.noreply.github.com>

--- Scripts/tornApiTest.ps1
+++
@@ -1,8 +0,0 @@
-﻿$response = Invoke-WebRequest -Uri https://api.torn.com/faction/?selections="&"key= -UseBasicParsing
-$factionInfo = $response | ConvertFrom-Json
-foreach ($member in $factionInfo.members)
-{    
-    $memberInfo = Invoke-WebRequest -Uri https://api.torn.com/user/"$member"?selections="&"key= -UseBasicParsing
-    $userInfo = $memberInfo.Content | ConvertFrom-Json
-    $userInfo
-}
